bdc04ef00e54b48dfc4b7c8a6e79c23635c5746b
hieunm <hieunm@zyyx.jp>
2024-11-24 22:22:16 +0700

    ui: home ui

--- mobile/lib/pages/application/home/home.dart
+++ mobile/lib/pages/application/home/home.dart
@@ -1,14 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+    return Scaffold(
+      backgroundColor: const Color(0xfff6f6f6),
+      appBar: AppBar(
+        backgroundColor: const Color(0xfff6f6f6),
+        toolbarHeight: 0,
+        scrolledUnderElevation: 0.0,
       ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/p.jpg', 
+                        width: 50, 
+                        height: 50, 
+                        fit: BoxFit.cover
+                      ),
+                    ),
+
+                    const SizedBox(width: 10),
+
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey
+                          ),
+                        ),
+                        Text(
+                          'Minh Hieu',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+
+                const Icon(
+                  Iconsax.notification_outline,
+                  size: 24,
+                )
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                color: const Color(0xfffbc700),
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Your coin list',
+                  style: TextStyle(
+                    fontSize: 14
+                  ),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12
+                  ),
+                )
+              ],
+            ),
+
+            const SizedBox(height: 5),
+
+            SizedBox(
+              height: 120,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (context, index) => const SizedBox(width: 10), 
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                  );
+                }
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Recommended for you',
+                  style: TextStyle(
+                    fontSize: 14
+                  ),
+                )
+              ],
+            ),
+
+            const SizedBox(height: 5),
+
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                child: ListView.separated(
+                itemCount: 10,
+                separatorBuilder: (context, index) => const SizedBox(height: 10),
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                  );
+                }
+                            ),
+              ))
+          ],
+        ),
+      )
     );
   }
 }
