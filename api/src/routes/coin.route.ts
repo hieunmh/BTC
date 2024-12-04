c3eeb0f44d0c01a857caec1af624c18d8c3316a4
hieunm <hieunm@zyyx.jp>
2024-12-05 00:05:21 +0700

    m

--- api/src/routes/coin.route.ts
+++
@@ -1,8 +0,0 @@
-import {  Router } from 'express';
-import * as coinController from '../controllers/coin.controller';
-
-const router: Router  = Router();
-
-router.get('/coin', coinController.getCoinMarket);
-
-export default router;
--- api/src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as userController from '../controllers/user.controller';
-import { isAuth } from "../middleware/auth";
-
-const router: Router  = Router();
-
-router.get('/user', isAuth, userController.getUser);
-
-export default router;  
